f64cbf886a6e33ba04e1f1d3d5345465fea00c2c
Daniel Giani <dnsgiani@gmail.com>
2024-07-24 00:05:12 -0300

    returning after a long time, fixing my cmake

--- main.cpp
+++ main.cpp
@@ -25,6 +25,35 @@ std::string ReadFile(const char* FilePath)
 	return FileContents;
 }
 
+void CheckShader(GLuint ShaderId)
+{
+	// ShaderId tem que ser um identificador de um shader já compilado
+	
+	GLint Result = GL_TRUE;
+	glGetShaderiv(ShaderId, GL_COMPILE_STATUS, &Result);
+
+	if (Result == GL_FALSE)
+	{
+		// Houve um erro ao compilar o shader, vamos impimir o log
+		// para saber qual foi o erro
+
+		// Obter o tamanho do log
+		GLint InfoLogLength = 0;
+		glGetShaderiv(ShaderId, GL_INFO_LOG_LENGTH, &InfoLogLength);
+
+		if (InfoLogLength > 0)
+		{
+			std::string ShaderInfoLog(InfoLogLength, '\0');
+			glGetShaderInfoLog(ShaderId, InfoLogLength, nullptr, &ShaderInfoLog[0]);
+			
+			std::cout << "Erro no shader" << std::endl;
+			std::cout << ShaderInfoLog << std::endl;
+			
+			assert(false);
+		}
+	}
+}
+
 GLuint LoadShaders(const char* VertexShaderFile, const char* FragmentShaderFile)
 {
 	std::string VertexShaderSource = ReadFile(VertexShaderFile);
@@ -41,33 +70,40 @@ GLuint LoadShaders(const char* VertexShaderFile, const char* FragmentShaderFile)
 	const char* VertexShaderSourcePtr = VertexShaderSource.c_str();
 	glShaderSource(VertexShaderId, 1, &VertexShaderSourcePtr, nullptr);
 	glCompileShader(VertexShaderId);
-
-	//Verificar se a compilação do vertex shader deu certo
+	CheckShader(VertexShaderId);
 
 	std::cout << "Compilando " << FragmentShaderFile << std::endl;
 	const char* FragmentShaderSourcePtr = FragmentShaderSource.c_str();
 	glShaderSource(FragmentShaderId, 1, &FragmentShaderSourcePtr, nullptr);
 	glCompileCommandListNV(FragmentShaderId);
-
-	//Verificar se a compilação do fragment shader deu certo
-
+	CheckShader(FragmentShaderId);
+	
 	std::cout << "Linkando o programa" << std::endl;
 	GLuint ProgramId = glCreateProgram();
 	glAttachShader(ProgramId, VertexShaderId);
 	glAttachShader(ProgramId, FragmentShaderId);
 	glLinkProgram(ProgramId);
+	// Verificar o programa
 
-	//Verificar o programa
 	GLint Result = GL_TRUE;
 	glGetProgramiv(ProgramId, GL_LINK_STATUS, &Result);
 
 	if (Result == GL_FALSE)
 	{
-		std::cout << "Erro ao linkar o programa" << std::endl;
 
-		//Pegar o log para avaliar problema
+		GLint InfoLogLength = 0;
+		glGetProgramiv(ProgramId, GL_INFO_LOG_LENGTH, &InfoLogLength);
 
-		assert(false);
+		if (InfoLogLength > 0)
+		{
+			std::string ProgramInfoLog(InfoLogLength, '\0');
+			glGetProgramInfoLog(ProgramId, InfoLogLength, nullptr, &ProgramInfoLog[0]);
+
+			std::cout << "Erro ao linkar o programa" << std::endl;
+			std::cout << ProgramInfoLog << std::endl;
+			
+			assert(false);
+		}
 	}
 
 	glDetachShader(ProgramId, VertexShaderId);
@@ -81,26 +117,26 @@ GLuint LoadShaders(const char* VertexShaderFile, const char* FragmentShaderFile)
 
 int main()
 {
-	//Inicializar a biblioteca GLFW
+	// Inicializar a biblioteca GLFW
 	assert(glfwInit() == GLFW_TRUE);
 
-	//Criar uma janela
+	// Criar uma janela
 	GLFWwindow* Window = glfwCreateWindow(Width, Height, "Blue Marble", nullptr, nullptr);
 	assert(Window);
 
 	glfwMakeContextCurrent(Window);
 
-	//Inicializar a biblioteca GLEW
+	// Inicializar a biblioteca GLEW
 	assert(glewInit() == GLEW_OK);
 
-	//Verificar a versão do OpenGL que está sendo usado
+	// Verificar a versão do OpenGL que está sendo usado
 	GLint GLMajorVersion = 0;
 	GLint GLMinorVersion = 0;
 	glGetIntegerv(GL_MAJOR_VERSION, &GLMajorVersion);
 	glGetIntegerv(GL_MINOR_VERSION, &GLMinorVersion);
 	std::cout << "OpenGL Version: " << GLMajorVersion << "." << GLMinorVersion << std::endl;
 
-	//Usar o glGetString() para obter informações do driver que está sendo usado
+	// Usar o glGetString() para obter informações do driver que está sendo usado
 	std::cout << "OpenGL Vendor: " << glGetString(GL_VENDOR) << std::endl;
 	std::cout << "OpenGL Renderer: " << glGetString(GL_RENDERER) << std::endl;
 	std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
@@ -108,30 +144,30 @@ int main()
 
 	GLuint ProgramId = LoadShaders("shaders/triangle_vert.glsl", "shaders/triangle_frag.glsl");
 
-	//Definir um triângulo em coordenadas normalizadas
+	// Definir um triângulo em coordenadas normalizadas
 	std::array<glm::vec3, 3> Triangle = {
 		glm::vec3{ -1.0f, -1.0f, 0.0f },
 		glm::vec3{  1.0f, -1.0f, 0.0f },
 		glm::vec3{  0.0f,  1.0f, 0.0f }
 	};
 
-	//Model
+	// Model
 	glm::mat4 ModelMatrix = glm::identity<glm::mat4>();
 
-	//View Matrix
+	// View Matrix
 	glm::vec3 Eye{ 0, 0, 5 };
 	glm::vec3 Center{ 0, 0, 0 };
 	glm::vec3 Up{ 0, 1, 0 };
 	glm::mat4 ViewMatrix = glm::lookAt(Eye, Center, Up);
 
-	//Projection Matrix
+	// Projection Matrix
 	constexpr float FoV = glm::radians(45.0f);
 	const float AspectRatio = Width / Height;
 	const float Near = 0.001f;
 	const float Far = 1000.f;
 	glm::mat4 ProjectionMatrix = glm::perspective(FoV, AspectRatio, Near, Far);
 
-	//ModelViewProjection
+	// ModelViewProjection
 	glm::mat4 ModelViewProjection = ProjectionMatrix * ViewMatrix * ModelMatrix;
 
 	//Aplicar a ModelViewProjection nos vértices do triângulo
@@ -142,55 +178,55 @@ int main()
 		Vertex = ProjectedVertex;
 	}
 
-	//Copiar os vértices do triângulo para a memória da GPU
+	// Copiar os vértices do triângulo para a memória da GPU
 	GLuint VertexBuffer;
 
-	//Pedir pro OpenGL gerar o identificador do VertexBuffer
+	// Pedir pro OpenGL gerar o identificador do VertexBuffer
 	glGenBuffers(1, &VertexBuffer);
 
 	//Ativar o VertexBuffer para onde serão copiados os dados do triângulo
 	glBindBuffer(GL_ARRAY_BUFFER, VertexBuffer);
 
-	//Copiar os dados para a memória de vídeo
+	// Copiar os dados para a memória de vídeo
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Triangle), Triangle.data(), GL_STATIC_DRAW);
 
-	//Definir a cor de fundo
+	// Definir a cor de fundo
 	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 
-	//Entrar no loop de eventos da aplicação
+	// Entrar no loop de eventos da aplicação
 	while (!glfwWindowShouldClose(Window))
 	{
-		//glClear vai limpar o framebuffer. GL_COLOR_BUFFER_BIT diz para limpar o buffer de cor. Após limpar irá preencher com a cor configurada no glClearColor
+		// glClear vai limpar o framebuffer. GL_COLOR_BUFFER_BIT diz para limpar o buffer de cor. Após limpar irá preencher com a cor configurada no glClearColor
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glEnableVertexAttribArray(0);
 
-		//Diz para o OpenGL que o VertexBuffer vai ser o buffer ativo no momento
+		// Diz para o OpenGL que o VertexBuffer vai ser o buffer ativo no momento
 		glBindBuffer(GL_ARRAY_BUFFER, VertexBuffer);
 
-		//Informa ao OpenGL onde, dentro do VertexBuffer se encontrarão os vértices
+		// Informa ao OpenGL onde, dentro do VertexBuffer se encontrarão os vértices
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-		//Diz ao OpenGL para desenhar o triângulo com os dados armazenados no VertexBuffer
+		// Diz ao OpenGL para desenhar o triângulo com os dados armazenados no VertexBuffer
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		//Reverter o estado
+		// Reverter o estado
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 		glDisableVertexAttribArray(0);
 
-		//Processar todos os eventos da fila de eventos do GLFW
-		//Podem ser eventos como: teclado, mouse, gamepad...
+		// Processar todos os eventos da fila de eventos do GLFW
+		// Podem ser eventos como: teclado, mouse, gamepad...
 		glfwPollEvents();
 
-		//Enviar o conteúdo do framebuffer da janela para ser desenhado na tela
+		// Enviar o conteúdo do framebuffer da janela para ser desenhado na tela
 		glfwSwapBuffers(Window);
 	}
 
-	//Desalocar o VertexBuffer
+	// Desalocar o VertexBuffer
 	glDeleteBuffers(1, &VertexBuffer);
 
-	//Encerrar a biblioteca GLFW
+	// Encerrar a biblioteca GLFW
 	glfwTerminate();
 
 	return 0;
